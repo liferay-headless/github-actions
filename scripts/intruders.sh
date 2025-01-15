ab163f7ed6d052dc101ebe730262274cc43bc5de
Carlos Correa <carlos.correa@liferay.com>
2025-01-15 08:12:46 +0100

    Petteri is not a intruder

--- scripts/intruders.sh
+++ scripts/intruders.sh
@@ -1,5 +1,5 @@
 grep '@liferay-headless' .github/CODEOWNERS | awk '{print $1}' | while read -r folder; do
-    commits=$(git log $1..HEAD --pretty=format:'%h %ae %s' -- "$folder" | grep -v -E 'brian.chan@liferay.com|alejandro.tardin@liferay.com|alberto.moreno@liferay.com|jalber786@gmail.com|carlos.correa@liferay.com|daniel.raposo@liferay.com|daniel.szimko@liferay.com|jaime.leon@liferay.com|magdalena.jedraszak@liferay.com|miklos.zakanyi@liferay.com|vendel.toreki@liferay.com|mauricio.valdivia@liferay.com|movaldivia@uc.cl|gabor.komaromi@liferay.com')
+    commits=$(git log $1..HEAD --pretty=format:'%h %ae %s' -- "$folder" | grep -v -E 'brian.chan@liferay.com|alejandro.tardin@liferay.com|alberto.moreno@liferay.com|jalber786@gmail.com|carlos.correa@liferay.com|daniel.raposo@liferay.com|daniel.szimko@liferay.com|jaime.leon@liferay.com|magdalena.jedraszak@liferay.com|miklos.zakanyi@liferay.com|vendel.toreki@liferay.com|mauricio.valdivia@liferay.com|movaldivia@uc.cl|gabor.komaromi@liferay.com|petteri.karttunen@liferay.com')
     if [ -n "$commits" ]; then
         echo "#### Intruders in $folder"
         echo "$commits"
