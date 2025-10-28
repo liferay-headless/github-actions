d13d3efa56d1a071505d8bb18d734eaccdb728c7
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-10-28 15:57:24 +0100

    Needed in ubuntu

--- scripts/analyze-testray-results.sh
+++ scripts/analyze-testray-results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname -- "${BASH_SOURCE[0]}")/testray_automation_tasks"
-python -m venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
