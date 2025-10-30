ac1abdf05ed41f428402285b2a15b9cc0ae214bb
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-10-30 05:08:20 +0100

    Moving to uv (https://docs.astral.sh/uv)

--- scripts/analyze-testray-results.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname -- "${BASH_SOURCE[0]}")/python"
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python3 analyze_testray_results.py
